5fd0d03a552906e1b739d3f3842d2e8be05e4227
Damian <dellacortedamian@gmail.com>
2023-04-10 12:35:20 -0300

    changes

--- script.js
+++ script.js
@@ -94,26 +94,56 @@ function typingEffect(text = '', time = 80, label = '') {
 typingEffect('Hi, I am a Full Stack', 80, typing);
 
 // Animated Bar
+window.addEventListener('scroll', function() {
+  var upSections = document.querySelectorAll('.up-section'); // Selecionamos todas las secciones
 
+  for (var i = 0; i < upSections.length; i++) {
+    var upSectionPosition = upSections[i].getBoundingClientRect().top;
+    var screenPosition = window.innerHeight / 1.3;
+
+    if (upSectionPosition < screenPosition) {
+      upSections[i].style.opacity = "1";
+      upSections[i].style.transform = "translateY(0)";
+
+      // Agregar la clase 'invisible' a cada elemento .cont__bar si no lo tiene
+      var contBars = upSections[i].querySelectorAll('.cont__bar');
+      for (var j = 0; j < contBars.length; j++) {
+        if (contBars[j].classList.contains('invisible')) {
+          contBars[j].classList.remove('invisible');
+        }
+      }
+    } else {
+      // Eliminar la clase 'invisible' de cada elemento .cont__bar si lo tiene
+      var contBars = upSections[i].querySelectorAll('.cont__bar');
+      for (var j = 0; j < contBars.length; j++) {
+        if (!contBars[j].classList.contains('invisible')) {
+          contBars[j].classList.add('invisible');
+        }
+      }
+    }
+  }
+});
 
 // Counter Porcent Bar
-// Para cada barra de progreso, incrementa el valor en un ciclo
 bars.forEach(bar => {
   const percentNumber = bar.querySelector('.percent-number');
   const barLength = parseInt(bar.querySelector('.bar').classList[1].slice(5));
   let percent = 0;
-  const intervalTime = barLength / 100 * 2000; // Calcula el tiempo del intervalo basado en el porcentaje de la barra
-  setTimeout(() => { // Agrega un delay antes de comenzar la animación
-    const interval = setInterval(() => {
-      percentNumber.innerText = `${percent}%`;
-      bar.querySelector('.bar').style.setProperty('--bar-lenght', `${percent}%`);
-      percent++;
-      if (percent > barLength) {
-        clearInterval(interval);
-      }
-    }, intervalTime / barLength); // Ajusta el intervalo en función del tiempo calculado
-  }, 2000);
+  const animationDuration = 2000; // Duración de la animación en milisegundos
+  const startTime = performance.now(); // Momento en que se inició la animación
+  const interval = setInterval(() => {
+    const currentTime = performance.now(); // Tiempo transcurrido desde el inicio de la animación
+    const elapsedTime = currentTime - startTime;
+    percent = Math.min(barLength, Math.floor((elapsedTime / animationDuration) * barLength));
+    percentNumber.innerText = `${percent}%`;
+    bar.querySelector('.bar').style.setProperty('--bar-lenght', `${percent}%`);
+    if (percent >= barLength) {
+      clearInterval(interval);
+    }
+  }, 16); // Intervalo de actualización en milisegundos (aproximadamente 60 fps)
 });
+
+
 
 // Portfolio Filter 
 filterContainer.addEventListener("click", (event) => {
